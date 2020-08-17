a0b86ad078b441b848ffb65f817aa6d8ce598d06
marcio lira <machollanda@gmail.com>
2020-08-17 19:18:11 -0300

    componente inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,17 @@ import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +30,8 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule, 
-    MatListModule 
+    MatListModule,
+    MatCardModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
