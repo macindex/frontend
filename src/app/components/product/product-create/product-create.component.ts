5857de77d7d1d07ad81a73e2565b14e716c038de
marcio lira <machollanda@gmail.com>
2020-08-19 23:50:46 -0300

    implementado formulário

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from './../product.service';
@@ -8,13 +9,21 @@ import { ProductService } from './../product.service';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
+    product: Product = {
+    name: '',
+    price: null
+  }
 
   constructor(private productService: ProductService, private router: Router){ }
 
   ngOnInit(): void {
   }
   createProduct(): void{
-    this.productService.showMessage('Produto criado')
+    // this.productService.showMessage('Produto criado')
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
   }
     cancel(): void{
     this.router.navigate(['/products'])
