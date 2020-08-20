384a1206a5a596bc382c82e9c3212b235d50d42a
marcio lira <machollanda@gmail.com>
2020-08-20 09:32:24 -0300

    componente obter produto

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -25,4 +25,7 @@ showMessage(msg: string): void{
 	create(product: Product): Observable<Product>{
 		return this.http.post<Product>(this.baseUrl, product)
 	}
+	read(): Observable<Product[]>{
+		return this.http.get<Product[]>(this.baseUrl)
+	}
 }
