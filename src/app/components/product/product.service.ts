ef8cafb5f6bba7c99da9b5b44311d7fac87233a8
marcio lira <machollanda@gmail.com>
2020-09-01 13:18:45 -0300

    exibicao de mensagem erro

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { Product } from './product.model';
-import { Observable } from 'rxjs';
+import { Observable, EMPTY } from 'rxjs';
 
 import { HttpClient } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,15 +16,23 @@ export class ProductService {
 
 constructor(private snackBar: MatSnackBar, private http: HttpClient){ }
 
-showMessage(msg: string): void{
-		this.snackBar.open(msg, 'X', {
+showMessage(msg: string, isError: boolean = false): void{
+	this.snackBar.open(msg, 'X', {
 	duration: 3000,
 	horizontalPosition: "right",
-	verticalPosition: "top"
-		})
-	}
+	verticalPosition: "top",
+	panelClass: isError ? ['msg-error'] : ['msg-success']
+	});
+}
 	create(product: Product): Observable<Product>{
-		return this.http.post<Product>(this.baseUrl, product)
+		return this.http.post<Product>(this.baseUrl, product).pipe(
+			map((obj) => obj),
+			catchError((e) => this.errorHandler(e))
+			);
+	}
+	errorHandler(e: any): Observable<any>{
+		this.showMessage('Ocorreu um erro!', true);
+		return EMPTY;
 	}
 	read(): Observable<Product[]>{
 		return this.http.get<Product[]>(this.baseUrl)
